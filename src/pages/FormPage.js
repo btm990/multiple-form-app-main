f70cdfa2018c8d85fefcfdfbe94c236b00fb8e95
btm990 <banelem101@gmail.com>
2023-07-08 00:29:45 +0200

    API endpoints hosted non-locally

--- src/pages/FormPage.js
+++ src/pages/FormPage.js
@@ -24,7 +24,7 @@ const handleSubmit = (e, setFormSubmitted) => {
         }
     }
 
-    const baseUrl = "http://localhost:8000/"
+    const baseUrl = "https://027d-105-184-236-56.ngrok-free.app/"
     const url_users = baseUrl + "api/users/"
     const url_subscription = baseUrl + "api/subscriptions/"
 
